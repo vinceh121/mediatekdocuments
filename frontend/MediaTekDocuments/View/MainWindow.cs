ed3a06aa155b0c6f0bfa83206afceba627d6e200
vinceh121 <contact@vinceh121.me>
2024-03-30 21:36:42 +0100

    style: format

--- frontend/MediaTekDocuments/View/MainWindow.cs
+++ frontend/MediaTekDocuments/View/MainWindow.cs
@@ -5,54 +5,54 @@ using UI = Gtk.Builder.ObjectAttribute;
 
 namespace MediaTekDocuments.View
 {
-	public class MainWindow : ApplicationWindow
-	{
-		private readonly Program _program;
+    public class MainWindow : ApplicationWindow
+    {
+        private readonly Program _program;
 
-		[UI]
-		private ComboBox _genreCombo;
-		[UI]
-		private TreeView _bookList;
+        [UI]
+        private ComboBox _genreCombo;
+        [UI]
+        private TreeView _bookList;
 
-		public MainWindow(Program program) : this(program, new Builder("MainWindow.glade")) { }
+        public MainWindow(Program program) : this(program, new Builder("MainWindow.glade")) { }
 
-		private MainWindow(Program program, Builder builder) : base(builder.GetRawOwnedObject("MainWindow"))
-		{
-			this._program = program;
-			this.Application = program.GetApplication();
-			builder.Autoconnect(this);
+        private MainWindow(Program program, Builder builder) : base(builder.GetRawOwnedObject("MainWindow"))
+        {
+            this._program = program;
+            this.Application = program.GetApplication();
+            builder.Autoconnect(this);
 
-			DeleteEvent += Window_DeleteEvent;
+            DeleteEvent += Window_DeleteEvent;
 
-			this.FillData();
-		}
+            this.FillData();
+        }
 
-		private async void FillData()
-		{
-			var genres = await Access.GetInstance().GetAllGenres();
-			ListStore genresModel = new(GLib.GType.String, GLib.GType.String);
+        private async void FillData()
+        {
+            var genres = await Access.GetInstance().GetAllGenres();
+            ListStore genresModel = new(GLib.GType.String, GLib.GType.String);
 
-			foreach (Genre g in genres)
-			{
-				genresModel.AppendValues(g.Id, g.Libelle);
-			}
+            foreach (Genre g in genres)
+            {
+                genresModel.AppendValues(g.Id, g.Libelle);
+            }
 
-			this._genreCombo.Model = genresModel;
-			
-			CellRendererText txtRender = new();
-			this._genreCombo.PackStart(txtRender, true);
-			this._genreCombo.SetAttributes(txtRender, "id", 0);
-			this._genreCombo.AddAttribute(txtRender, "text", 1);
+            this._genreCombo.Model = genresModel;
 
-			this._genreCombo.IdColumn = 1;
-			this._genreCombo.Active = 0;
+            CellRendererText txtRender = new();
+            this._genreCombo.PackStart(txtRender, true);
+            this._genreCombo.SetAttributes(txtRender, "id", 0);
+            this._genreCombo.AddAttribute(txtRender, "text", 1);
 
-			this._genreCombo.Sensitive = true;
-		}
+            this._genreCombo.IdColumn = 1;
+            this._genreCombo.Active = 0;
 
-		private void Window_DeleteEvent(object sender, DeleteEventArgs a)
-		{
-			Application.Quit();
-		}
-	}
+            this._genreCombo.Sensitive = true;
+        }
+
+        private void Window_DeleteEvent(object sender, DeleteEventArgs a)
+        {
+            Application.Quit();
+        }
+    }
 }
